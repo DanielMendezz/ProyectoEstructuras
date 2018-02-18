62b87a1a1e1f784dd8b1e515b58f2c03188b4253
Unknown <D1007702164@hotmail.com>
2018-02-17 22:39:44 -0500

    Commit ejemplo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,9 +13,15 @@
     </head>
     <body>
         <div class="container">
-        <!-- Content here -->
-        <h1>Estructura de Datos</h1>
+  <!-- Content here -->
+  Estructuras
+  <div class="row">
+         <div class="col-sm-8">Hola Mundo</div>
+         <div class="col-sm-4">Bienvenidos a mi pagina web</div>
        </div>
+</div>
+      
+
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
